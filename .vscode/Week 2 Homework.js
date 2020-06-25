49ebca89d6581b6bcea6743dbc07653540f1dcd6
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-06-24 22:17:53 -0500

    Upload Week 2 Homework

---
+++ .vscode/Week 2 Homework.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Pizza Please";
+let numberOfToppings = 11;
+
+console.log(typeof pizzaPlace, pizzaPlace);
+console.log(typeof numberOfToppings, numberOfToppings);
+
+console.log("Welcome to " + pizzaPlace + "! We have " + numberOfToppings + " different toppings for you to choose from, and the first one's on us!")
+
+if (numberOfToppings < 10) {
+    console.log("Quality, not quantity");
+} else {
+        console.log("A whole lot of pizza");
+    };
