8624d61892e6f6907e957a69f85701e456329382
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-06-24 22:32:18 -0500

    Updating Week 2 Homework with Bonus

--- .vscode/Week 2 Homework.js
+++ .vscode/Week 2 Homework.js
@@ -11,3 +11,10 @@ if (numberOfToppings < 10) {
 } else {
         console.log("A whole lot of pizza");
     };
+
+
+for(i=1; i<=numberOfToppings; i++){
+    if(i % 2 == 0){
+    console. log(i);
+    }
+    }
