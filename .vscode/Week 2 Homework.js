de6dce96dc77b55aec98e0fb851003bb99a322a4
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-06-24 22:41:52 -0500

    Update Week 2 Homework

--- .vscode/Week 2 Homework.js
+++ .vscode/Week 2 Homework.js
@@ -4,17 +4,17 @@ let numberOfToppings = 11;
 console.log(typeof pizzaPlace, pizzaPlace);
 console.log(typeof numberOfToppings, numberOfToppings);
 
-console.log("Welcome to " + pizzaPlace + "! We have " + numberOfToppings + " different toppings for you to choose from, and the first one's on us!")
+console.log("Welcome to " + pizzaPlace + "! We have " + numberOfToppings + " different toppings for you to choose from, and the first one's on us!");
 
 if (numberOfToppings < 10) {
     console.log("Quality, not quantity");
 } else {
-        console.log("A whole lot of pizza");
-    };
+    console.log("A whole lot of pizza");
+};
 
 
 for(i=1; i<=numberOfToppings; i++){
     if(i % 2 == 0){
-    console. log(i);
+        console. log(i);
     }
-    }
+}
